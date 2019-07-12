b3aabf628bf6b2a3794de0b61246c0084379d9ae
Abhishek Chakraborty <abhishek.y2a@gmail.com>
2019-07-12 19:18:51 +0530

    KoolTelecomApiGateway First Commit

---
+++ src/main/java/com/gateway/api/telecom/kool/KoolTelecomApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.gateway.api.telecom.kool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class KoolTelecomApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoolTelecomApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateway/api/telecom/kool/KoolTelecomApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gateway.api.telecom.kool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KoolTelecomApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
