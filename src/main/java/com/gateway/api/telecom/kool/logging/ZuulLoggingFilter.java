c07d31701ea35a2e9c338ae660f565ea8e27d189
Abhishek Chakraborty <abhishek.y2a@gmail.com>
2019-07-18 20:06:54 +0530

    logging changes incorporated

--- src/main/java/com/gateway/api/telecom/kool/logging/ZuulLoggingFilter.java
+++ src/main/java/com/gateway/api/telecom/kool/logging/ZuulLoggingFilter.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
+@Component
 public class ZuulLoggingFilter extends ZuulFilter{
 	
 	private Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
@@ -21,7 +23,8 @@ public class ZuulLoggingFilter extends ZuulFilter{
 	@Override
 	public Object run() throws ZuulException {
 		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
-		logger.info("request -> {}, requestUri -> {}, requestUrl -> {}", request, request.getRequestURI(), request.getRequestURL());
+		//logger.info("Logging from ZUUL API GATEWAY , request -> {}, requestUri -> {}, requestUrl -> {}", request, request.getRequestURI(), request.getRequestURL());
+		logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
 		return null;
 	}
 
